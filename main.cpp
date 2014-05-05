a2a86ccdf507ecb7ebd0ccd3f56a770dfe9d8b66
Frander <frandergv@gmail.com>
2014-05-04 23:21:49 -0600

    "Se cambia, al anterior se le metio chuky!!!!"

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ string letras(string letra) {
     for (int i = 0; i < 2; i++) {
         if (letra == diccinario[i]) {
             return listaLetras[i];
-        }   
+        }
     }
     return letra;
 }
@@ -26,23 +26,24 @@ int main() {
     string letra;
     string lett;
     stringstream l;
-    char buf[255];
+    int bits;
+    char buf[11];
     int fd = open("/dev/ttyACM0", O_RDONLY);
     if (fd == -1) {
         printf("El puerto no esta disponible");
     } else {
         int res;
         while (true) {
-            while ((res = read(fd, buf, 255)) == 0);
+            while ((res = read(fd, buf, bits)) == 0);
             {
                 if (res > 0) {
                     buf[res] = 0;
-                    l << buf;
-                    l >> letra;
-                    lett += letra; 
+                    letra += buf;
                 }
-                cout << letras(lett) << endl;
-                lett="";
+                if (letra.size() == 12) {
+                    cout << letras(letra) << endl;
+                    letra = "";
+                }
             }
         }
     }
